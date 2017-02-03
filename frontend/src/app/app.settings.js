0749ac6029f3d31f0fce5150be21649cde0a6c9c
Anna Pohorielova <anya.pogorelova@gmail.com>
2017-02-03 15:49:58 +0200

    Removed src/**/*.js files

--- frontend/src/app/app.settings.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var AppSettings = (function () {
-    function AppSettings() {
-    }
-    AppSettings.API_ENDPOINT = 'http://localhost:50451/api/';
-    return AppSettings;
-}());
-exports.AppSettings = AppSettings;
-//# sourceMappingURL=app.settings.js.map
