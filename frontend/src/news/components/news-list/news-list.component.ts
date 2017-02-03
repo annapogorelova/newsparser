1fadb0df22c8ff2b9b7b25477e8f89b9c4928c20
Anna Pohorielova <anya.pogorelova@gmail.com>
2017-02-03 16:11:03 +0200

    Fixed styles for the news list

--- frontend/src/news/components/news-list/news-list.component.ts
+++ frontend/src/news/components/news-list/news-list.component.ts
@@ -3,6 +3,7 @@ import {ApiService} from '../../../shared/services/api/api.service';
 
 @Component({
     templateUrl: './news-list.component.html',
+    styles: [require('./news-list.component.css').toString()]
 })
 export class NewsListComponent  {
     public news: any = [];
