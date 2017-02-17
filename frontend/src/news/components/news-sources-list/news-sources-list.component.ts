92e08573e8b0d2b4b7d5aeffe7d24a87ff73b3d8
Anna Pohorielova <anya.pogorelova@gmail.com>
2017-02-17 12:37:10 +0200

    Added news sources list component + some css fixes

---
+++ frontend/src/news/components/news-sources-list/news-sources-list.component.ts
@@ -0,0 +1,40 @@
+import {Component, Inject, Input} from '@angular/core';
+import {ApiService} from '../../../shared/services/api/api.service';
+
+@Component({
+    selector: 'news-sources',
+    templateUrl: './news-sources-list.component.html',
+    styles: [require('./news-sources-list.component.css').toString()]
+})
+
+/**
+ * Component for displaying the list of news sources
+ */
+export class NewsSourcesListComponent {
+    public newsSources: any = [{name: 'All', id: null}];
+    public selectedSourceId: number;
+
+    @Input() selectHandler: any = null;
+    @Input() initialSelectedSourceId: any = null;
+
+    constructor(private apiService: ApiService){
+        this.loadNewsSources();
+    }
+
+    loadNewsSources = () => {
+        this.apiService.get('newsSources').then(newsSources => this.handleLoadedNewsSources(newsSources));
+    };
+
+    handleLoadedNewsSources = (data: any) => {
+        this.newsSources = this.newsSources.concat(data);
+        this.selectedSourceId = this.initialSelectedSourceId;
+    };
+
+    onSelect = (source: any) => {
+        this.selectedSourceId = source.id;
+
+        if(this.selectHandler){
+            this.selectHandler(source);
+        }
+    };
+}
--- frontend/src/news/news.module.ts
+++ frontend/src/news/news.module.ts
@@ -4,10 +4,11 @@ import { NewsRouting, NewsRoutingProviders } from './news.routing';
 import { BrowserModule } from '@angular/platform-browser';
 import { InfiniteScrollModule } from 'angular2-infinite-scroll';
 import { SharedModule } from '../shared/shared.module';
+import { NewsSourcesListComponent } from './components/news-sources-list/news-sources-list.component';
 
 @NgModule({
     imports: [ NewsRouting, BrowserModule, InfiniteScrollModule, SharedModule ],
-    declarations: [ NewsListComponent ],
+    declarations: [ NewsListComponent, NewsSourcesListComponent ],
     providers: [ NewsRoutingProviders ]
 })
 export class NewsModule {}
