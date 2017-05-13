6c261d7a1688b4e78411c7cc9c236677c1424f97
anna <anya.pogorelova@gmail.com>
2017-05-13 15:53:57 +0300

    [FE] Reorganized /app directory .

--- frontend/src/app/components/app/app.component.ts
+++ frontend/src/app/components/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
-import {AuthService} from '../../shared/services/auth/auth.service';
-import {AuthProviderService} from '../../shared/services/auth/auth-provider.service';
+import {AuthService} from '../../../shared/services/auth/auth.service';
+import {AuthProviderService} from '../../../shared/services/auth/auth-provider.service';
 import {Router} from "@angular/router";
 
 @Component({
