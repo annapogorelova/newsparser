dba795880d027dc1878204f09522e0487f25fd60
anna <anya.pogorelova@gmail.com>
2017-06-23 22:28:23 +0300

    [BE] Added validation to AccountModel.

--- backend/newsparser.web/API/Models/AccountModel.cs
+++ backend/newsparser.web/API/Models/AccountModel.cs
@@ -1,8 +1,13 @@
-﻿namespace NewsParser.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewsParser.API.Models
 {
     public class AccountModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
         public string Email { get; set; }
         public bool HasPassword { get; set; }
     }
