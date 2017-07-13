a954dfd347ef49e6a59ba40c7ac4d8eee661f2b5
anna <anya.pogorelova@gmail.com>
2017-07-13 13:46:05 +0300

    [FE] Added a webpack compression plugin for production build.

--- frontend/config/webpack.prod.js
+++ frontend/config/webpack.prod.js
@@ -2,6 +2,7 @@ var webpack = require('webpack');
 var webpackMerge = require('webpack-merge');
 var commonConfig = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var CompressionPlugin = require('compression-webpack-plugin');
 var helpers = require('./helpers');
 
 module.exports = webpackMerge(commonConfig, {
@@ -19,6 +20,13 @@ module.exports = webpackMerge(commonConfig, {
                 minimize: false // workaround for ng2
             }
         }),
-        new UglifyJSPlugin()
+        new UglifyJSPlugin(),
+        new CompressionPlugin({
+            asset: '[path].gz[query]',
+            algorithm: 'gzip',
+            test: /\.(js|html)$/,
+            threshold: 10240,
+            minRatio: 0.8
+        })
     ]
 });
