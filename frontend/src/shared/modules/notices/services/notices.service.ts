a099491ef076eb462f2e201801cc0d9cd5cd2e7f
anna <anya.pogorelova@gmail.com>
2017-07-19 13:47:35 +0300

    [FE] Updated notices module.

--- frontend/src/shared/modules/notices/services/notices.service.ts
+++ frontend/src/shared/modules/notices/services/notices.service.ts
@@ -3,48 +3,62 @@ import {Notice} from '../models';
 
 @Injectable()
 export class NoticesService {
-	protected notices: Array<Notice> = [];
-	public deleteTimeout: number = 4000;
-	public deleteInterval: number = 42;
+    protected notices:Array<Notice> = [];
+    public deleteTimeout:number = 5000;
+    public deleteInterval:number = 42;
 
-	constructor() {
-		// #browser-specific
-		if (typeof (window) !== 'undefined') {
-			setInterval(() => this.shiftAfterDeleteTimeout(), this.deleteInterval);
-		}
-	}
+    constructor() {
+        // #browser-specific
+        if (typeof (window) !== 'undefined') {
+            setInterval(() => this.deleteOutdated(), this.deleteInterval);
+        }
+    }
 
-	success(title: string, text?: string): void {
-		this.notices.push(new Notice(Math.floor(Date.now()), 'success', title, text))
-	}
+    exists(newNotice:Notice):boolean {
+        return Boolean(this.notices.filter((notice:Notice) => notice.isEqual(newNotice)).length);
+    }
 
-	warning(title: string, text?: string): void {
-		this.notices.push(new Notice(Math.floor(Date.now()), 'warning', title, text))
-	}
+    success(title:string, text?:string):void {
+        const newNotice = new Notice(Math.floor(Date.now()), 'success', title, text);
+        if (!this.exists(newNotice)) {
+            this.notices.push(newNotice);
+        }
+    }
 
-	error(title: string, text?: string): void {
-		this.notices.push(new Notice(Math.floor(Date.now()), 'error', title, text))
-	}
+    warning(title:string, text?:string):void {
+        const newNotice = new Notice(Math.floor(Date.now()), 'warning', title, text);
+        if (!this.exists(newNotice)) {
+            this.notices.push(newNotice);
+        }
+    }
 
-	info(title: string, text?: string): void {
-		this.notices.push(new Notice(Math.floor(Date.now()), 'info', title, text))
-	}
+    error(title:string, text?:string):void {
+        const newNotice = new Notice(Math.floor(Date.now()), 'error', title, text);
+        if (!this.exists(newNotice)) {
+            this.notices.push(newNotice);
+        }
+    }
 
-	get(): Array<Notice> {
-		return this.notices;
-	}
+    info(title:string, text?:string):void {
+        const newNotice = new Notice(Math.floor(Date.now()), 'info', title, text);
+        if (!this.exists(newNotice)) {
+            this.notices.push(newNotice);
+        }
+    }
 
-	deleteByIndex(index: number): void {
-		this.notices = this.notices.filter((element, elementIndex) => elementIndex !== index);
-	}
+    get():Array<Notice> {
+        return this.notices;
+    }
 
-	deleteAll(): void {
-		this.notices = [];
-	}
+    deleteAll():void {
+        this.notices = [];
+    }
 
-	shiftAfterDeleteTimeout(): void {
-		if (this.notices.length && this.notices[0].timestampCreated + this.deleteTimeout < Math.floor(Date.now())) {
-			this.notices.shift();
-		}
-	}
+    deleteByIndex(index:number):void {
+        this.notices = this.notices.filter((element, elementIndex) => elementIndex !== index);
+    }
+
+    deleteOutdated():void {
+        this.notices = this.notices.filter((notice:Notice) => notice.createdAt + this.deleteTimeout > Math.floor(Date.now()));
+    }
 }
